dada08e6de92e9241deb02f35d53c64cff4c1542
董俊 <418826102@qq.com>
2019-09-05 23:20:57 +0800

    发表权限验证

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,11 +9,21 @@
             </div>
             <div class="card-body">
                 <div class="form-group">
-                    <textarea class="form-control" name="content" id="content" rows="3" style="resize: none"></textarea>
+                    @auth
+                        <textarea class="form-control" name="content" id="content" rows="3"
+                                  style="resize: none"></textarea>
+                    @else
+                        <textarea disabled class="form-control" name="content" id="content" rows="3"
+                                  style="resize: none"></textarea>
+                    @endauth
                 </div>
             </div>
             <div class="card-footer text-muted text-right">
-                <button class="btn btn-primary btn-sm">发表</button>
+                @auth
+                    <button class="btn btn-primary btn-sm">发表</button>
+                @else
+                    <a href="{{route('login')}}" class="btn btn-primary btn-sm">请先登录</a>
+                @endauth
             </div>
         </div>
     </form>
